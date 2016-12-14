c8d29bcfa486f1e0dee753dce7f0b0cea9d43d36
Winsor <winsor08@mail.ru>
2016-12-14 08:51:02 +0200

    small build script change

--- prepare_builds.cmd
+++ prepare_builds.cmd
@@ -11,8 +11,8 @@ REM @timeout /T 5 /NOBREAK
 @set @zipper="D:\Program Files\7-Zip\7z.exe"
 @del /F /Q d:\Temp\bin_Release.7z
 @call %@zipper% a d:\Temp\bin_Release.7z y:\bin_Release\ -xr!*.ilk  
-@del /F /Q d:\Temp\bin_Debug.7z
-@call %@zipper% a d:\Temp\bin_Debug.7z y:\bin_Debug\ -xr!*.ilk  
+rem @del /F /Q d:\Temp\bin_Debug.7z
+rem @call %@zipper% a d:\Temp\bin_Debug.7z y:\bin_Debug\ -xr!*.ilk  
 @timeout /T 5 /NOBREAK 
 
 @echo user winsor> ftpcmd.dat
@@ -20,9 +20,9 @@ REM @timeout /T 5 /NOBREAK
 @echo bin>> ftpcmd.dat
 @echo cd ~/public_html>> ftpcmd.dat
 @echo del bin_Release.7z>> ftpcmd.dat
-@echo del bin_Debug.7z>> ftpcmd.dat
+rem @echo del bin_Debug.7z>> ftpcmd.dat
 @echo put d:\Temp\bin_Release.7z>> ftpcmd.dat
-@echo put d:\Temp\bin_Debug.7z>> ftpcmd.dat
+rem @echo put d:\Temp\bin_Debug.7z>> ftpcmd.dat
 @echo by>> ftpcmd.dat
 @ftp -n -s:ftpcmd.dat fss.ukrhub.net
 @del ftpcmd.dat
